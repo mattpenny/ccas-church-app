d8762f3c1e49ac936b94a41e7dcb702a23f1afe2
mattpenny <matt.cheang@gmail.com>
2026-09-05 20:26:48 +0800

    Configure app to load from server URL for automatic updates

--- android/app/src/main/java/com/ccas/churchapp/MainActivity.java
+++ android/app/src/main/java/com/ccas/churchapp/MainActivity.java
@@ -1,5 +1,13 @@
 package com.ccas.churchapp;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Load from server URL for latest version
+        bridge.getWebView().loadUrl("https://ccac-api.ccac-church.workers.dev");
+    }
+}
